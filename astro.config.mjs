4f39e0edf9081c08f3dd36c4059e18a984fc37db
Andrii Chemerysov <achemerysov@gmail.com>
2026-04-26 16:59:10 -0400

    Add astro.config.mjs and update package.json dependencies

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
+    integrations: [react()],
+
+    // pre-built HTML (with support for interactive islands)
+    // as per 2026-04-16-hybrid-as-frontend-rendering.txt
+    output: 'static',
+
+    // folder name, here 'dist/_assets/'
+    build: {
+        assets: '_assets',
+    },
+});
